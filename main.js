ef4c4316051e94765a75e33b7f2e56802ebec8b2
Rupesh Siva Sai <105511559+rupeshsivasai@users.noreply.github.com>
2025-02-23 12:05:19 +0530

    Add mobile menu toggle with icon click event listener

---
+++ main.js
@@ -0,0 +1,7 @@
+let icon = document.querySelector(".icon");
+let ul = document.querySelector("ul");
+
+icon.addEventListener("click", () => {
+    ul.classList.toggle("showData");
+    console.log(ul)
+})
